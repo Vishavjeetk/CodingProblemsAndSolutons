929ee716ff4ef540e9b2d599f627c7437f027f70
Vishavjeet Singh <vishavjeetsingh348@gmail.com>
2024-09-05 07:40:19 +0530

    Added Solution for Length Of Last Word in a string

---
+++ lengthOfLastWord.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class lengthOfLastWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = "Hello World";
+        int result = lengthOfLastWordSolution(string);
+        System.out.println(result);
+        sc.close();
+    }
+
+    public static int lengthOfLastWordSolution(String s) {
+        int counter = 0;
+        int previousCount = 0;
+        boolean start = false;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s.charAt(i) == ' ') {
+                if (start == true) break;
+                counter = 0;
+                continue;
+            }
+            start = true;
+            counter++;
+            previousCount = counter;
+        }
+        return previousCount;
+    }
+}
